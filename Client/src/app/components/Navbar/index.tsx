8d741a92f4d3cfb14ffdf00d5343852e934052e4
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-03-28 17:33:25 -0400

    initial commit

---
+++ Client/src/app/components/Navbar/index.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import React from "react";
+import { Menu, Bell, Sun, Settings, Moon } from "lucide-react";
+import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/app/state";
+
+function Navbar() {
+  const dispatch = useAppDispatch();
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const toggleSideBar = () => {
+    dispatch(setIsSidebarCollapsed(!isSideBarCollapsed));
+  };
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
+
+  return (
+    <div className="flex justify-between items-center w-full mb-7">
+      {/* LEFT SIDE */}
+      <div className="flex justify-between items-center gap-5 ">
+        <button
+          className="px-3 py-3 bg-gray-100 rounded hover:bg-blue-100 dark:bg-gray-900 dark:hover:bg-black dark:border-1"
+          onClick={toggleSideBar}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+
+        <div className="relative">
+          <input
+            type="search"
+            placeholder="Start type to search groups & products"
+            className="pl-10 py-2 pr-4 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500 dark:bg-black"
+          />
+          <div className="absolute inset-y-0 left-0 flex pl-3 items-center pointer-events-none">
+            <Bell className="text-gray-500" size={20} />
+          </div>
+        </div>
+      </div>
+
+      {/* RIGHT SIDE */}
+      <div className="flex justify-between items-center gap-5">
+        <div className="hidden md:flex justify-between items-center gap-5">
+          <div>
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
+            </button>
+          </div>
+          <div className="relative">
+            <Bell className="cursor-pointer text-gray-500" size={24} />
+            <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
+              3
+            </span>
+          </div>
+          <hr className="w-0 h-7 border-solid border-l border-gray-300 mx-3" />
+          <div className="flex items-center gap-3 cursor-pointer">
+            <div className="w-9 h-9">image</div>
+            <span className="font-semibold">Joel Nguyen</span>
+          </div>
+        </div>
+        <Link href="/settings">
+          <Settings className="cursor-pointer text-gray-500" size={24} />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
