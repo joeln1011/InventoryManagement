72ac87e836c21aef1d8bf5d71965e6eee4479cc2
Joel Nguyen <joelnguyen@Joels-MacBook-Pro-2.local>
2025-04-12 15:19:15 -0400

    initial commit

--- Server/ecosystem.config.js
+++ Server/ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  app: [
+  apps: [
     {
       name: "InventoryManagement",
       script: "npm",
       args: "run dev",
       env: {
         NODE_ENV: "development",
-        ENV_VAR1: "environment_variable",
+        ENV_VAR1: "environment-variable",
       },
     },
   ],
